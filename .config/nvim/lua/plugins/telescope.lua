0d6651aa8b027e7ee4299f1a6bd3b5923be4ac38
Kyle Muldoon <kylemuldoon15@gmail.com>
2024-12-20 12:06:15 -0800

    add updates from work laptop

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,58 +2,74 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      "nvim-telescope/telescope-file-browser.nvim",
+      {
+        "nvim-telescope/telescope-frecency.nvim",
+        dependencies = { "kkharji/sqlite.lua" },
+      },
+    },
     opts = {
       defaults = {
-        file_ignore_patterns = {
-          -- Node modules
-          "node_modules/.*",
-          -- Git directories
-          "%.git/.*",
-          -- Build directories
-          "build/.*",
-          "dist/.*",
-          -- Cache directories
-          "%.cache/.*",
-          -- Package lock files
-          "package%-lock.json",
-          "yarn.lock",
-          -- Minified files
-          "%.min.js",
-          "%.min.css",
-          -- Log files
-          "%.log",
-          -- Dependencies
-          "vendor/.*",
-          -- Environment files
-          "%.env.*",
+        file_sorter = require("telescope.sorters").get_fuzzy_file,
+        generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+        path_display = { "truncate" },
+
+        cache_picker = {
+          num_pickers = 5,
+          limit_entries = 1000,
         },
-        -- Additional default configurations
-        -- path_display = { "truncate" },
-        -- layout_config = {
-        --   horizontal = {
-        --     preview_width = 0.55,
-        --     results_width = 0.8,
-        --   },
-        --   vertical = {
-        --     mirror = false,
-        --   },
-        --   width = 0.87,
-        --   height = 0.80,
-        --   preview_cutoff = 120,
-        -- },
       },
-      -- Picker-specific configurations can be added here
+
       pickers = {
         find_files = {
-          hidden = true, -- Show hidden files
-          no_ignore = true, -- Don't respect .gitignore
+          find_command = {
+            "fd",
+            "--type",
+            "f", -- Only files
+            "--strip-cwd-prefix", -- Clean output for telescope
+            "--hidden", -- Show hidden files
+            "--no-ignore-vcs", -- Don't respect .gitignore
+            "--exclude",
+            ".git", -- Explicitly exclude .git
+            "--exclude",
+            "node_modules", -- Explicitly exclude node_modules
+            "--exclude",
+            "**/dist/**", -- Exclude dist directories at any depth
+            "--exclude",
+            "**/.next/**", -- Exclude .next directories at any depth
+          },
         },
+
         live_grep = {
           additional_args = function()
-            return { "--hidden" }
+            return {
+              "--hidden", -- Show hidden files
+              "--no-ignore-vcs", -- Don't respect .gitignore
+              "--max-columns",
+              "150",
+              "--glob",
+              "!.git/*", -- Exclude .git directory
+              "--glob",
+              "!node_modules/*", -- Exclude node_modules directory
+              "--glob",
+              "!**/dist/**", -- Exclude dist directories at any depth
+              "--glob",
+              "!**/.next/**", -- Exclude .next directories at any depth
+            }
           end,
         },
       },
     },
+    config = function(_, opts)
+      local telescope = require("telescope")
+      telescope.setup(opts)
+
+      -- Load extensions
+      telescope.load_extension("fzf")
+      telescope.load_extension("file_browser")
+      telescope.load_extension("frecency")
+    end,
   },
 }
